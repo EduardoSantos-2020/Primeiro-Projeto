7c60d5e29a2c253590a0da469aa57b5b5e9d5464
EduardoSantos-2020 <eduardosantos123444@gmail.com>
2026-08-17 23:20:08 -0300

    ajuste teste pagina pedido campo login

--- js/ScriptPedidos.js
+++ js/ScriptPedidos.js
@@ -39,7 +39,7 @@ $(window).ready(() => {
          if ($('.userGrup').hasClass('exitUser')) {
             $(btn.currentTarget).css({ 'width': '14%' });
          } else {
-            $(btn.currentTarget).css({ 'width': '100%' });
+            $(btn.currentTarget).css({ 'width': '85%' });
          }
       } else {
          if ($('.userGrup').hasClass('exitUser')) {
